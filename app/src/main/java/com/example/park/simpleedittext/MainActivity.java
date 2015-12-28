1f9eef9b47f3d3c367a182df3539c1c9872b11d8
park <adsloader99@gmail.com>
2015-12-28 10:25:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/park/simpleedittext/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.park.simpleedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt = (EditText)findViewById(R.id.editText);
+        edt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    Toast.makeText(getApplicationContext(), edt.getText().toString(), Toast.LENGTH_LONG).show();
+                    edt.setText("");
+                }
+            }
+        });
+
+    }
+}
